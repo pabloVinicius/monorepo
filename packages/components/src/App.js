4d51bc01089ffc7ba34ee6ede26e574bb516c2af
Pablo Vinícius <vinicius.pablo.18@gmail.com>
2019-12-23 11:36:45 -0300

    setting up react native to work with monorepo

---
+++ packages/components/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+        <Text>Monorepo component</Text>
+    </View>
+  );
+};
+
+export default App;
